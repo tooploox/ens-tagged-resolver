de88995c3322d147a1e6f4cdf379212bd1229e22
Mariusz Żak <mariuszzak21@gmail.com>
2018-05-21 15:30:20 +0200

    Initial version of PublicResolver

---
+++ contracts/PublicResolver.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.24;
+
+import "github.com/OpenZeppelin/zeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract PublicResolver is Ownable {
+    event AddrSet(bytes32 indexed node, address a, bytes32 tag);
+
+    mapping (bytes32 => mapping (bytes32 => address)) records;
+
+    constructor() public {}
+
+    function setAddr(bytes32 node, address addr) public onlyOwner {
+        setAddr(node, addr, 'latest');
+    }
+
+    function setAddr(bytes32 node, address addr, bytes32 tag) public onlyOwner {
+        records[node][tag] = addr;
+        emit AddrSet(node, addr, tag);
+    }
+
+    function addr(bytes32 node, bytes32 tag) public view returns (address) {
+        return records[node][tag];
+    }
+
+    function supportsInterface(bytes4 interfaceID) public pure returns (bool) {
+        return true;
+    }
+}
