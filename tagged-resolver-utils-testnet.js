28290c2af9d49bf6bf3e13e8069c11bd2e0460be
Mariusz Żak <mariuszzak21@gmail.com>
2018-05-28 08:45:43 +0200

    Adjust utils to new contract's ABI

--- tagged-resolver-utils-testnet.js
+++ tagged-resolver-utils-testnet.js
@@ -9,136 +9,185 @@ var taggedResolverUtils = {
     }
     return node.toString();
   },
-  PublicResolver: web3.eth.contract([{
-    "constant": true,
-    "inputs": [],
-    "name": "owner",
-    "outputs": [{
-      "name": "",
-      "type": "address"
-    }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  }, {
-    "constant": false,
-    "inputs": [{
-      "name": "newOwner",
-      "type": "address"
-    }],
-    "name": "transferOwnership",
-    "outputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-  }, {
-    "inputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "constructor"
-  }, {
-    "anonymous": false,
-    "inputs": [{
-      "indexed": true,
-      "name": "node",
-      "type": "bytes32"
-    }, {
-      "indexed": false,
+  PublicResolver: web3.eth.contract([
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "owner",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "transferOwnership",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "node",
+          "type": "bytes32"
+        },
+        {
+          "indexed": false,
+          "name": "addr",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "name": "tag",
+          "type": "bytes32"
+        }
+      ],
+      "name": "AddrSet",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "previousOwner",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "OwnershipTransferred",
+      "type": "event"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "node",
+          "type": "bytes32"
+        },
+        {
+          "name": "addr",
+          "type": "address"
+        }
+      ],
+      "name": "setAddr",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "node",
+          "type": "bytes32"
+        },
+        {
+          "name": "addr",
+          "type": "address"
+        },
+        {
+          "name": "tag",
+          "type": "bytes32"
+        }
+      ],
+      "name": "setAddrForTag",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "node",
+          "type": "bytes32"
+        }
+      ],
       "name": "addr",
-      "type": "address"
-    }, {
-      "indexed": false,
-      "name": "tag",
-      "type": "bytes32"
-    }],
-    "name": "AddrSet",
-    "type": "event"
-  }, {
-    "anonymous": false,
-    "inputs": [{
-      "indexed": true,
-      "name": "previousOwner",
-      "type": "address"
-    }, {
-      "indexed": true,
-      "name": "newOwner",
-      "type": "address"
-    }],
-    "name": "OwnershipTransferred",
-    "type": "event"
-  }, {
-    "constant": false,
-    "inputs": [{
-      "name": "node",
-      "type": "bytes32"
-    }, {
-      "name": "addr",
-      "type": "address"
-    }, {
-      "name": "tag",
-      "type": "bytes32"
-    }],
-    "name": "setAddr",
-    "outputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-  }, {
-    "constant": false,
-    "inputs": [{
-      "name": "node",
-      "type": "bytes32"
-    }, {
-      "name": "addr",
-      "type": "address"
-    }],
-    "name": "setAddr",
-    "outputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-  }, {
-    "constant": true,
-    "inputs": [{
-      "name": "node",
-      "type": "bytes32"
-    }],
-    "name": "addr",
-    "outputs": [{
-      "name": "",
-      "type": "address"
-    }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  }, {
-    "constant": true,
-    "inputs": [{
-      "name": "node",
-      "type": "bytes32"
-    }, {
-      "name": "tag",
-      "type": "bytes32"
-    }],
-    "name": "addr",
-    "outputs": [{
-      "name": "",
-      "type": "address"
-    }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  }]),
-  setAddr: function setAddr(resolverAddress, domain, address, tag) {
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "node",
+          "type": "bytes32"
+        },
+        {
+          "name": "tag",
+          "type": "bytes32"
+        }
+      ],
+      "name": "addrForTag",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ]),
+  setAddr: function setAddr(resolverAddress, domain, address) {
     var node = this.namehash(domain);
     var resolver = this.PublicResolver.at(resolverAddress);
 
-    return resolver.setAddr(node, address, tag, { from: web3.eth.accounts[0] });
+    return resolver.setAddr(node, address, { from: web3.eth.accounts[0] });
   },
-  getAddr: function getAddr(resolverAddress, domain, tag) {
+  setAddrForTag: function setAddr(resolverAddress, domain, address, tag) {
     var node = this.namehash(domain);
     var resolver = this.PublicResolver.at(resolverAddress);
 
-    return resolver.addr(node, tag, { from: web3.eth.accounts[0] });
+    return resolver.setAddrForTag(node, address, tag, { from: web3.eth.accounts[0] });
+  },
+  getAddr: function getAddr(resolverAddress, domain) {
+    var node = this.namehash(domain);
+    var resolver = this.PublicResolver.at(resolverAddress);
+
+    return resolver.addr(node, { from: web3.eth.accounts[0] });
+  },
+  getAddrForTag: function getAddr(resolverAddress, domain, tag) {
+    var node = this.namehash(domain);
+    var resolver = this.PublicResolver.at(resolverAddress);
+
+    return resolver.addrForTag(node, tag, { from: web3.eth.accounts[0] });
   }
 };
